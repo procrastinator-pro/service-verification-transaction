714a076557670a0edca1a0fd88b4628771b0b700
sudhanshu kumar <jaykaran76@gmail.com>
2024-01-10 19:53:28 +0530

    Added pom

---
+++ src/main/java/com/cashless/service/verification/transaction/serviceverificationtransaction/ServiceVerificationTransactionApplication.java
@@ -0,0 +1,13 @@
+package com.cashless.service.verification.transaction.serviceverificationtransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceVerificationTransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceVerificationTransactionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cashless/service/verification/transaction/serviceverificationtransaction/ServiceVerificationTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cashless.service.verification.transaction.serviceverificationtransaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceVerificationTransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
